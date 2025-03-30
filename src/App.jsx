dd2499959159957baa6b3e22a87e5745f2fc8800
PiyushGoyal02 <goyalp3542@gmail.com>
2025-03-30 23:54:12 +0530

    create the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
+import './App.css';
 
-import './App.css'
-
-function App (){
+function App() {
+  const pages = ["Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6"];
+  
   return (
-    <div>
+    <div className="main-box">
+      {/* Inside contact box */}
+      <div className="inside-contact-box">
+        <div className="allpages-checkbox">
+          <label className="allpages-text" htmlFor="myCheckbox">All pages</label>
+          <input className="checkbox" type="checkbox" id="myCheckbox" />
+        </div>
 
-      {/* main box */}
-      <div className='main-box'>
-        
-        
+        <hr className="HR-tag" />
+
+        <div className="Pages-section">
+          {pages.map((page) => (
+            <div key={page}>
+              <label className="">
+                <input type="checkbox" /> {page}
+              </label>
+            </div>
+          ))}
+        </div>
+
+        <hr className="HR-tag" />
+
+        <div className="button-container">
+          <button className="Done-button">Done</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
